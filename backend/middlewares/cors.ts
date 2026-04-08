deb131ee9ff2ba78ffc1014d01a93d8118ddf9c7
KandukuriAmar <2200030959@kluniversity.in>
2026-04-08 16:48:35 +0530

    remove some unwanted code lines

--- backend/middlewares/cors.ts
+++ backend/middlewares/cors.ts
@@ -1,20 +1,6 @@
 import cors from "cors";
 
 const corsMiddleware = cors({
-  origin: (origin, callback) => {
-    const allowedOrigins = [
-      "http://localhost:3000",
-      "http://localhost:3001",
-      "http://127.0.0.1:3000",
-      "http://127.0.0.1:3001",
-    ];
-    
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
   origin: "http://localhost:3000",
   credentials: true,
 });
